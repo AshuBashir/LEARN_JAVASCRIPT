18a86863822fa48529299d1f6d58949b93489c76
Ashu <aashubashir@gmail.com>
2022-03-15 11:02:29 +0530

    commit

--- dot&bracket-obj.js
+++ dot&bracket-obj.js
@@ -1,6 +1,16 @@
+// difference between dot and bracket
+const key = "email";  // email can be any email adress
 const person ={
     name:"Ashu",
     age:24,
-    "person hobbies": ["Pampering Birds", "Gardening" , "Learning about religional facts"] //spaces arent allowed in JS, but using "" , we can create a
+    "person hobbies": ["Pampering Birds", "Gardening" , "Learning about religional facts"] //spaces arent allowed in JS, but using "" , we can create a key with multiple words.
 }
- console.log(person.person hobbies);
+ console.log(person["person hobbies"]);  //multiple word key cant be accessed by dot , instead use [""] for mutliple keys.
+
+// to add key in a person
+
+// person.key = "aashubashir@gmail.com";  //to print key of a person
+// console.log(person);
+ 
+person[key] = "aashubashir@gmail.com";  //to print key of a person
+console.log(person);
